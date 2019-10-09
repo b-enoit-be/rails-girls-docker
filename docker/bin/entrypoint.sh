00c9f661663769a80c0a16eaa3fc93cb52df22a6
Benoît Geeraerts <benn-be@users.noreply.github.com>
2019-10-09 23:07:00 +0200

    Update entrypoint.sh

--- docker/bin/entrypoint.sh
+++ docker/bin/entrypoint.sh
@@ -4,7 +4,6 @@ set -e
 if [ ! -f /data/rails-girls/Gemfile ]; then
   bundle config disable_platform_warnings true
   rails new . --database=postgresql
-  rails webpacker:install
   rake db:create
 fi  
 
